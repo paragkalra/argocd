713d17a15716e996f13a775877f2837334d0ab07
paragkalra <paragkalra@gmail.com>
2026-05-27 07:09:15 -0700

    Added a script showing argocd login, password update and other related commands.

---
+++ udemy-argocd-and-argo-rollout/argocd_login.sh
@@ -0,0 +1,9 @@
+brew install argocd
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+argocd login localhost:8080 --name local
+argocd context
+argocd account get-user-info
+argocd account update-password
+argocd login localhost:8080
+argocd help
+argocd app list
