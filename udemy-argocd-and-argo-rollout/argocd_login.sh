c4280c5f860b12121e2085f121179943c5275ea9
paragkalra <paragkalra@gmail.com>
2026-05-27 07:14:53 -0700

    Added command to logout as well.

--- udemy-argocd-and-argo-rollout/argocd_login.sh
+++ udemy-argocd-and-argo-rollout/argocd_login.sh
@@ -1,9 +1,10 @@
 brew install argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-argocd login localhost:8080 --name local
+argocd login localhost:8080
 argocd context
 argocd account get-user-info
 argocd account update-password
 argocd login localhost:8080
 argocd help
 argocd app list
+argocd logout localhost:8080
